dc4032d9734ee9072234d4972b8026994df332a0
JukeBoxHeroTP <lenzo1gaming@gmail.com>
2018-11-11 22:59:25 -0600

    add express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/tutorials', express.static(__dirname + '/tutorials'));
+app.use('/songpresets', express.static(__dirname + '/songpresets'));
+app.use('/help', express.static(__dirname + '/help'));
+app.use('/css', express.static(__dirname + '/css'));
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
